bee4c2e2d6a0d4700f2540b02edc3383da8b60b6
peterxichen <peterxichen@aol.com>
2021-04-11 15:48:11 -0700

    list operations

--- collection_scripts.scala
+++ collection_scripts.scala
@@ -32,8 +32,32 @@ object CollectionScripts{
             }
         }
         // lists end with an instance of Nil as terminus
-        while (i != Nil) { print(i.head + ", "; i = i.tail)}
+        println(List())
+        val nillist = List()
+        println(nillist == Nil)
+        val single_list: List[String] = List("o")
+        println(single_list.tail)
+        while (i != Nil) { print(i.head + ", "); i = i.tail }
 
+        // cons operator
+        val nums = 1 :: 2 :: 3 :: Nil
+        println(nums)
+        println(Nil.::(10))
+        println(2 :: nums)
+
+        // list arithmetic
+        // List is immutable, operations return new List
+        println(List(1, 2, 3, 4) filter (_ > 2))
+        println(List(1, 2, 3, 4) partition (_ < 3))
+        println(List("hello", "goodbye", "hellogoodbye") sortBy (_.size))
+
+        // corollary operations
+        val nums2 = List(1, 2, 3, 4, 5, 6, 7, 8)
+        println(nums2 :+ 9)
+        println(nums2 takeRight 3)
+        println(nums2 dropRight 3)
+
+        // Set
         val colorset = Set("red", "red", "green")
         println(colorset)
 
@@ -52,6 +76,8 @@ object CollectionScripts{
         println(cMap("red") | cMap("green"))
         println(cMap.contains("green"))
         for (c <- cMap) { println(c) }
+
+        println("\nMapping lists")
 
         println("\nTerminating...")
     }

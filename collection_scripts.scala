ff8facb9c2a4661db13e7ac07590c170590c966d
peterxichen <peterxichen@aol.com>
2021-04-11 11:39:03 -0700

    map/reduce examples

--- collection_scripts.scala
+++ collection_scripts.scala
@@ -2,17 +2,29 @@
 
 object CollectionScripts{
     def main(args: Array[String]): Unit = {
-        println("List, Sets, Maps")
+        println("List, Sets")
 
-        val color_list = List("red", "green", "blue", "purple")
-        println(color_list)
-        println(color_list.head)
-        println(color_list(1))
+        val colors = List("red", "green", "blue", "purple")
+        println(colors)
+        println(colors.head)
+        println(colors(1))
         var x: Int = 1
-        for (c <- color_list) { 
+        for (c <- colors) { 
             println(x.toString + ' ' + c)
             x+=1
         }
+
+        val colorset = Set("red", "red", "green")
+        println(colorset)
+
+        println("\nforeach, map, reduce")
+        colors.foreach( (c: String) => println(c))
+        val sizes = colors.map( (c: String) => c.size)
+        println(sizes)
+        val sizes_plus_1 = colors.map( (c: String) => (c.size, 1))
+        println(sizes_plus_1)
+        val total = sizes.reduce( (a: Int, b: Int) => a + b)
+        println(total)
 
         println("\nTerminating...")
     }
